4fbb861e1cd54836cea6a9e73208a8a981a7bb66
Safal Pandita <safalpandita@gmail.com>
2019-03-03 03:14:18 +0530

    Add: real working beyblades with physics

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,43 +4,57 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public enum axis
+    {
+        x,y,z
+    }
+
     public Rigidbody rb;
 
     public int movementSpeed = 1000;
-    public int angularVelocity = 1000;
+    public long angularVelocity = int.MaxValue;
+    public axis rotationAxis;
     public Controls.KeysetName controls;
     private Keyset ks;
+    private Vector3 rotationVector;
+    public int mass;
 
     // Start is called before the first frame update
     void Start()
     {
         ks = Controls.GetKeyset(controls);
+        if (rotationAxis.Equals(axis.x)) rotationVector = Vector3.right;
+        else if (rotationAxis.Equals(axis.y)) rotationVector = Vector3.up;
+        else if (rotationAxis.Equals(axis.z)) rotationVector = Vector3.forward;
+        rb.mass = mass;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Quaternion deltaRotation = Quaternion.Euler(Vector3.up * angularVelocity * Time.deltaTime);
+        Quaternion deltaRotation = Quaternion.Euler(rotationVector * angularVelocity * Time.deltaTime);
         rb.MoveRotation(rb.rotation * deltaRotation);
+
+        rb.freezeRotation = true;
 
         if (Input.GetKey(ks.LEFT))
         {
-            rb.AddForce(Vector3.back * movementSpeed * Time.deltaTime);
+            rb.AddForce(Vector3.back * movementSpeed * Time.deltaTime, ForceMode.Acceleration);
 
         }
         if (Input.GetKey(ks.RIGHT))
         {
-            rb.AddForce(Vector3.forward * movementSpeed * Time.deltaTime);
+            rb.AddForce(Vector3.forward * movementSpeed * Time.deltaTime, ForceMode.Acceleration);
 
         }
         if (Input.GetKey(ks.UP))
         {
-            rb.AddForce(Vector3.left * movementSpeed * Time.deltaTime);
+            rb.AddForce(Vector3.left * movementSpeed * Time.deltaTime, ForceMode.Force);
 
         }
         if (Input.GetKey(ks.DOWN))
         {
-            rb.AddForce(Vector3.right * movementSpeed * Time.deltaTime);
+            rb.AddForce(Vector3.right * movementSpeed * Time.deltaTime, ForceMode.Force);
         }
 
     }
