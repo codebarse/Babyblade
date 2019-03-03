03f55428cc154b8b856df5e2d663a78da578a699
Pulkit Kumar Kariryaa <pulkitkariryaa@Pulkits-MacBook-Air.local>
2019-03-03 18:46:57 +0530

    Adding menu

---
+++ Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+	bool gameHasEnded = false;
+	int numberOfPlayers = 3;
+
+    public void EndGame()
+    {
+    	numberOfPlayers--;
+    	if(numberOfPlayers <= 1)
+        {
+        	Debug.Log("Game Over");
+        	Restart();
+        }
+    }
+
+    void Restart()
+    {
+
+    }
+}
