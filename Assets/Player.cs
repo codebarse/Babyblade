390f38b329577effa9bdd414c1d62a7a8761181e
Safal Pandita <safalpandita@gmail.com>
2019-03-03 20:12:10 +0530

    Add: dome + beyblade textures

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -18,6 +18,7 @@ public class Player : MonoBehaviour
     private Keyset ks;
     private Vector3 rotationVector;
     public int mass;
+    private ForceMode forceMode = ForceMode.Acceleration;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +28,7 @@ public class Player : MonoBehaviour
         else if (rotationAxis.Equals(axis.y)) rotationVector = Vector3.up;
         else if (rotationAxis.Equals(axis.z)) rotationVector = Vector3.forward;
         rb.mass = mass;
+        Physics.gravity = new Vector3(1,-9.8F, 1);
     }
 
     // Update is called once per frame
@@ -43,22 +45,21 @@ public class Player : MonoBehaviour
 
         if (Input.GetKey(ks.LEFT))
         {
-            rb.AddForce(Vector3.back * movementSpeed * Time.deltaTime, ForceMode.Acceleration);
+            rb.AddForce(Vector3.back * movementSpeed * Time.deltaTime, forceMode);
 
         }
         if (Input.GetKey(ks.RIGHT))
         {
-            rb.AddForce(Vector3.forward * movementSpeed * Time.deltaTime, ForceMode.Acceleration);
-
+            rb.AddForce(Vector3.forward * movementSpeed * Time.deltaTime, forceMode);
         }
         if (Input.GetKey(ks.UP))
         {
-            rb.AddForce(Vector3.left * movementSpeed * Time.deltaTime, ForceMode.Force);
+            rb.AddForce(Vector3.left * movementSpeed * Time.deltaTime, forceMode);
 
         }
         if (Input.GetKey(ks.DOWN))
         {
-            rb.AddForce(Vector3.right * movementSpeed * Time.deltaTime, ForceMode.Force);
+            rb.AddForce(Vector3.right * movementSpeed * Time.deltaTime, forceMode);
         }
 
     }
